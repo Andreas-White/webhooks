30297449e2e675ce3bba59365fed299c7e7eb681
Andreas Manos <antmanos42@gmail.com>
2022-01-09 17:43:26 +0100

    trying post request v04

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,14 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  console.log('Received webhook get request to /webhook-receive')
+  console.log('Request body:')
+  console.log(req.body)
   res.render('index', { title: 'Express' })
 })
 
 router.post('/', function (req, res, next) {
-  console.log('Received webhook request to /webhook-receive')
+  console.log('Received webhook post request to /webhook-receive')
   console.log('Request body:')
   console.log(req.body)
 })
