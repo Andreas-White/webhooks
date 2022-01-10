41285e3e9f56880d9d45b35a1696740776c1d666
Andreas Manos <antmanos42@gmail.com>
2022-01-10 01:53:47 +0100

    trying webhook request v02

--- controllers/webHookController.js
+++ controllers/webHookController.js
@@ -2,7 +2,7 @@ const webHook = (req, res) => {
   console.log('Received webhook get request')
 
   console.log('Request Head:')
-  console.log(req.head)
+  console.log(req.header)
 
   console.log('Request Body:')
   console.log(req.body)
