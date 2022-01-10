89f58e1b6f5a83ecf3df1d74864d724b9bb07669
Andreas Manos <antmanos42@gmail.com>
2022-01-10 02:34:29 +0100

    trying webhook request v13

--- controllers/webHookController.js
+++ controllers/webHookController.js
@@ -4,14 +4,8 @@ const webHook = (req, res) => {
   console.log('Request Headers:')
   console.log(req.headers)
 
-  // let headers = req.headers
-
-  // headers.forEach((head) => {
-  //   console.log(head + ' ----- ')
-  // })
-
-  // console.log('Request Headers token:')
-  // console.log(req.headers.x-gitlab-token)
+  console.log('Request Headers token:')
+  console.log(req.headers['x-gitlab-token'])
 
   console.log('Request Body:')
   console.log(req.body)
