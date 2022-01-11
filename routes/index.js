2f9b1fc12a80c6e1efeabaa43ce8d9d3026af6f0
Andreas Manos <antmanos42@gmail.com>
2022-01-11 03:39:18 +0100

    fetching all issues from gitlab

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
 import express from 'express'
 import controller from '../controllers/webHookController.js'
+import fetch from 'node-fetch'
 
 const router = express.Router()
 
 /* GET home page. */
 router.get('/', controller.webHook)
 
-// router.get('/', function(req, res) {
-//   res.json();
-// })
+router.get('/issue', async (req, res) => {
+  const url = 'https://gitlab.lnu.se/api/v4/projects/20151/issues'
+
+  const headers = {
+    'PRIVATE-TOKEN': 'AjkME5VHNem5mXYSSrAX',
+  }
+
+  let issues = await fetch(url, { method: 'GET', headers: headers })
+    .then((res) => res.json())
+    .catch((err) => console.log(err))
+
+  console.log(issues)
+  res.send(200)
+})
 
 export default router
