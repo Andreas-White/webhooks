4357822978378d89df360c4ef2e30edb5737627e
Andreas Manos <antmanos42@gmail.com>
2022-01-15 20:08:49 +0100

    fixing security with issues

--- controllers/webHookController.js
+++ controllers/webHookController.js
@@ -34,6 +34,12 @@ const issues = async (req, res) => {
     'PRIVATE-TOKEN': process.env.GITLAB_TOKEN,
   }
 
+  if (req.session.user !== user) {
+    req.session.flash =
+      'You have to login if already have an account or sign up to create one, in order to access forbidden resources'
+    res.sendFile(dir.join(__dirname, 'public', 'html', 'errors', '403.html'))
+  }
+
   let issues = await fetch(url, { method: 'GET', headers: headers })
     .then((res) => res.json())
     .catch((err) => console.log(err))
