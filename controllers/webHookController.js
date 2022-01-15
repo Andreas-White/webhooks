2dc3fa96be62e692510973cbd61d86ad1ee7ccb5
Andreas Manos <antmanos42@gmail.com>
2022-01-15 18:50:00 +0100

    using template engine and saving users credentials in mongodb atlas v03

--- controllers/webHookController.js
+++ controllers/webHookController.js
@@ -13,19 +13,19 @@ const webHook = (req, res) => {
 
   console.log('Request Body:')
   console.log(req.body)
-  // global.io.emit('webhook', req.body)
-  // global.io.emit('webhook-message', 'a new issue triggered')
+  global.io.emit('webhook', req.body)
+  global.io.emit('webhook-message', 'a new issue triggered')
 
   // res.json(req.body)
-  const user = req.params.user
+  // const user = req.params.user
 
-  if (secrettoken === req.headers['x-gitlab-token']) {
-    req.session.flash = 'A webhook was triggered by Gitlab'
-  } else {
-    req.session.flash = 'A webhook was triggered, but not from Gitlab'
-  }
+  // if (secrettoken === req.headers['x-gitlab-token']) {
+  //   req.session.flash = 'A webhook was triggered by Gitlab'
+  // } else {
+  //   req.session.flash = 'A webhook was triggered, but not from Gitlab'
+  // }
 
-  res.redirect(`/issues/${user}`)
+  // res.redirect(`/issues/${user}`)
 }
 
 const issues = async (req, res) => {
